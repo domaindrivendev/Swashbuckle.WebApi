c830ceaff34e59d110fd876f7459692b148b2816
rmorris <rmorris@gettyimages.com>
2013-10-18 13:07:40 -0700

    Fix bug around duplicate Id generation for generic types

---
+++ Swashbuckle.TestApp/Models/MyGenericType.cs
@@ -0,0 +1,10 @@
+﻿namespace Swashbuckle.TestApp.Models
+{
+    public class MyGenericType<T>
+    {
+        public string TypeName
+        {
+            get { return typeof (T).Name; }
+        }
+    }
+}
