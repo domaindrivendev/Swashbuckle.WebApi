bb139c1e8c0e45189bdb498d00d3781004ca6d79
domaindrivendev <domaindrivendev@gmail.com>
2014-04-28 12:45:21 -0700

    Add camel casing filter example

---
+++ Swashbuckle.Dummy.Core/SwaggerExtensions/ApplyCamelCasing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Swashbuckle.Swagger;
+
+namespace Swashbuckle.Dummy.SwaggerExtensions
+{
+    public class ApplyCamelCasing : IModelFilter
+    {
+        public void Apply(DataType model, DataTypeRegistry dataTypeRegistry, Type type)
+        {
+            var camelCasedProperties = new Dictionary<string, DataType>();
+            foreach (var entry in model.Properties)
+            {
+                var key = entry.Key;
+                camelCasedProperties[Char.ToLower(key[0]) + key.Substring(1)] = entry.Value;
+            }
+
+            model.Properties = camelCasedProperties;
+        }
+    }
+}
