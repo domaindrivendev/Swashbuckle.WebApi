0f95732ed7edc1703efeb63b7c57598d66471184
Chris Peterson <chris@sparkdev.com>
2015-01-05 10:20:16 -0800

    Add ability to hide model fields that are [Obsolete]

---
+++ Swashbuckle.Dummy.Core/Controllers/ObsoleteModelFieldsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Swashbuckle.Dummy.Controllers
+{
+    public class ObsoleteModelFieldsController : ApiController
+    {
+        static int _generatedId;
+
+        public int Create(CreateEntityForm form)
+        {
+            return _generatedId++;
+        }
+    }
+    
+    public class CreateEntityForm
+    {
+        [Obsolete("We switched to generated ids -- clients should only provide Name")]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
