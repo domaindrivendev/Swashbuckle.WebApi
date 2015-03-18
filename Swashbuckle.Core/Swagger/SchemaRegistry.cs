668b2aae0583425264320d4f5e89fbd3e521f201
Jody Justus <jjustus@gmail.com>
2015-03-18 14:56:26 -0500

    Changed to type.FullName to properly support nested types in schemas. e.g., Google.Apis.Storage.v1.Data.ObjectAccessControl+ProjectTeamData

--- Swashbuckle.Core/Swagger/SchemaRegistry.cs
+++ Swashbuckle.Core/Swagger/SchemaRegistry.cs
@@ -231,7 +231,7 @@ namespace Swashbuckle.Swagger
         {
             var typeName = type.Name;
             if (_useFullTypeNameInSchemaIds)
-                typeName = type.Namespace + "." + typeName;
+                typeName = type.FullName;
 
             if (type.IsGenericType)
             {
