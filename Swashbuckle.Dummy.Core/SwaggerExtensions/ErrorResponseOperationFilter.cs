5c0b13aac24b5134fb73a7be1e7d8dbe2857d291
Dickson Lui <dickson.lui@answerconsulting.com>
2015-03-30 14:58:24 +0100

    Added ErrorResponseOperationFilter and SwaggerResponseModelAttribute classes.
    Users can now create a SwaggerResponseModelAttribute and decorate end
    point and have swashbuckle generate the Models/schemas for Swagger.
    
    example usage:
    [SwaggerResponseModel("Controller_WithResponseModel", "400", "Description of 400 error", typeof(ModelState))]
    [SwaggerResponseModel("Controller_WithoutResponseModel", "500","Description of 500 error")]

---
+++ Swashbuckle.Dummy.Core/SwaggerExtensions/ErrorResponseOperationFilter.cs
@@ -0,0 +1,38 @@
+﻿namespace Swashbuckle.Dummy.SwaggerExtensions
+{
+    using System.Linq;
+    using System.Web.Http.Description;
+    using Swashbuckle.Swagger;
+
+    public class ErrorResponseOperationFilter : IOperationFilter
+    {
+
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+
+
+            var customAttributesList =
+                apiDescription.ActionDescriptor.GetCustomAttributes<SwaggerResponseModelAttribute>()
+                    .Where(x => x.OperationId == operation.operationId)
+                    .ToList();
+
+            if (customAttributesList.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var customAttributes in customAttributesList)
+            {
+                Schema schema = null;
+                if (customAttributes.ModelType != null)
+                {
+                    schemaRegistry.GetOrRegister(customAttributes.ModelType);
+                    schema = new Schema { @ref = "#/definitions/" + customAttributes.ModelType.Name };
+                }
+                operation.responses.Add(
+                    customAttributes.StatusCode,
+                    new Response { description = customAttributes.Description.Trim(), schema = schema });
+            }
+        }
+    }
+}
