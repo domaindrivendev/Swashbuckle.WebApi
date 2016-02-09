0807f1be7aaa38da75dd310d315b55fa131b5bb5
domaindrivendev <domaindrivendev@gmail.com>
2016-02-09 00:12:14 -0800

    Resolve issues (incl. failing tests) with 38b7b7e

--- Swashbuckle.Core/Swagger/SwaggerGenerator.cs
+++ Swashbuckle.Core/Swagger/SwaggerGenerator.cs
@@ -5,6 +5,7 @@ using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System.Net.Http.Formatting;
+using System.Net.Http;
 
 namespace Swashbuckle.Swagger
 {
@@ -124,18 +125,18 @@ namespace Swashbuckle.Swagger
             return pathItem;
         }
 
-        private Operation CreateOperation(ApiDescription apiDescription, SchemaRegistry schemaRegistry)
+        private Operation CreateOperation(ApiDescription apiDesc, SchemaRegistry schemaRegistry)
         {
-            var parameters = apiDescription.ParameterDescriptions
+            var parameters = apiDesc.ParameterDescriptions
                 .Select(paramDesc =>
                     {
-                        var inPath = apiDescription.RelativePathSansQueryString().Contains("{" + paramDesc.Name + "}");
-                        return CreateParameter(paramDesc, inPath, schemaRegistry);
+                        string location = GetParameterLocation(apiDesc, paramDesc);
+                        return CreateParameter(location, paramDesc, schemaRegistry);
                     })
                  .ToList();
 
             var responses = new Dictionary<string, Response>();
-            var responseType = apiDescription.ResponseType();
+            var responseType = apiDesc.ResponseType();
             if (responseType == null || responseType == typeof(void))
                 responses.Add("204", new Response { description = "No Content" });
             else
@@ -143,37 +144,39 @@ namespace Swashbuckle.Swagger
 
             var operation = new Operation
             { 
-                tags = new [] { _options.GroupingKeySelector(apiDescription) },
-                operationId = apiDescription.FriendlyId(),
-                produces = apiDescription.Produces().ToList(),
-                consumes = apiDescription.Consumes().ToList(),
+                tags = new [] { _options.GroupingKeySelector(apiDesc) },
+                operationId = apiDesc.FriendlyId(),
+                produces = apiDesc.Produces().ToList(),
+                consumes = apiDesc.Consumes().ToList(),
                 parameters = parameters.Any() ? parameters : null, // parameters can be null but not empty
                 responses = responses,
-                deprecated = apiDescription.IsObsolete()
+                deprecated = apiDesc.IsObsolete()
             };
 
             foreach (var filter in _options.OperationFilters)
             {
-                filter.Apply(operation, schemaRegistry, apiDescription);
+                filter.Apply(operation, schemaRegistry, apiDesc);
             }
 
             return operation;
         }
 
-        private Parameter CreateParameter(ApiParameterDescription paramDesc, bool inPath, SchemaRegistry schemaRegistry)
+        private string GetParameterLocation(ApiDescription apiDesc, ApiParameterDescription paramDesc)
         {
-            var supportsOnlyGetMethod = 
-                paramDesc.ParameterDescriptor.ActionDescriptor.SupportedHttpMethods.Contains(System.Net.Http.HttpMethod.Get) &&
-                paramDesc.ParameterDescriptor.ActionDescriptor.SupportedHttpMethods.Count == 1;
-            
-            var @in = (inPath)
-                ? "path"
-                : (paramDesc.Source == ApiParameterSource.FromUri || supportsOnlyGetMethod) ? "query" : "body";
+            if (apiDesc.RelativePathSansQueryString().Contains("{" + paramDesc.Name + "}"))
+                return "path";
+            else if (paramDesc.Source == ApiParameterSource.FromBody && apiDesc.HttpMethod != HttpMethod.Get)
+                return "body";
+            else
+                return "query";
+        }
 
+        private Parameter CreateParameter(string location, ApiParameterDescription paramDesc, SchemaRegistry schemaRegistry)
+        {
             var parameter = new Parameter
             {
-                name = paramDesc.Name,
-                @in = @in
+                @in = location,
+                name = paramDesc.Name
             };
 
             if (paramDesc.ParameterDescriptor == null)
@@ -183,7 +186,7 @@ namespace Swashbuckle.Swagger
                 return parameter; 
             }
 
-            parameter.required = inPath || !paramDesc.ParameterDescriptor.IsOptional;
+            parameter.required = location == "path" || !paramDesc.ParameterDescriptor.IsOptional;
             parameter.@default = paramDesc.ParameterDescriptor.DefaultValue;
 
             var schema = schemaRegistry.GetOrRegister(paramDesc.ParameterDescriptor.ParameterType);
