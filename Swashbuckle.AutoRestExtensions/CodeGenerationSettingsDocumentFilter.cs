5daf83eaefee502ba000e09c7df6d6ff6e1b7e73
Sean Fausett <sean@jellyfish.co.nz>
2016-07-17 14:55:02 +1200

    Add AutoRest extensions

---
+++ Swashbuckle.AutoRestExtensions/CodeGenerationSettingsDocumentFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace Swashbuckle.AutoRestExtensions
+{
+    public class CodeGenerationSettingsDocumentFilter : IDocumentFilter
+    {
+        public CodeGenerationSettingsDocumentFilter(object settings) // Pass an anonymous type for easy opt-in
+        {
+            if (settings == null)
+                throw new ArgumentNullException("settings");
+
+            _settings = settings;
+        }
+
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            // Add code generation settings
+            swaggerDoc.info.vendorExtensions.Add("x-ms-code-generation-settings", _settings);
+        }
+
+        private readonly object _settings;
+    }
+}
---
+++ Swashbuckle.AutoRestExtensions/SchemaExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Swashbuckle.Swagger;
+
+namespace Swashbuckle.AutoRestExtensions
+{
+    public static class SchemaExtensions
+    {
+        public static bool HasNullable(this Schema schema)
+        {
+            return (schema.vendorExtensions != null) &&
+                schema.vendorExtensions.ContainsKey("x-nullable");
+        }
+
+        public static bool IsNullable(this Schema schema)
+        {
+            object value;
+            return (schema.vendorExtensions != null) &&
+                schema.vendorExtensions.TryGetValue("x-nullable", out value) &&
+                (value is bool) &&
+                (bool)value;
+        }
+    }
+}
