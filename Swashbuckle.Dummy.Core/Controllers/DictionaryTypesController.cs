edea12f54c0f54f2eb7237018f24de4163cdbe44
Sean Fausett <sean@jellyfish.co.nz>
2016-07-18 14:32:24 +1200

    Add unit tests for AutoRest extensions

--- Swashbuckle.Dummy.Core/Controllers/DictionaryTypesController.cs
+++ Swashbuckle.Dummy.Core/Controllers/DictionaryTypesController.cs
@@ -11,6 +11,12 @@ namespace Swashbuckle.Dummy.Controllers
         {
             throw new NotImplementedException();
         }
+
+        [Route("term-definitions")]
+        public IDictionary<Term, string> UpdateTermDescriptions(IDictionary<Term, string> terms)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public enum Term
---
+++ Swashbuckle.Dummy.Core/Types/SimpleComposite.cs
@@ -0,0 +1,8 @@
+﻿namespace Swashbuckle.Dummy.Types
+{
+    public class SimpleComposite
+    {
+        public bool Boolean { get; set; }
+        public string String { get; set; }
+    }
+}
