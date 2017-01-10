4e71360bb48d7b0fff9ff090d10fe7d26d77460d
Mikael Brevik <mikaelbre@gmail.com>
2017-01-10 11:06:49 +0100

    Adds failing test for old version of Swagger UI

--- Swashbuckle.Dummy.Core/Controllers/FileDownloadController.cs
+++ Swashbuckle.Dummy.Core/Controllers/FileDownloadController.cs
@@ -5,14 +5,31 @@ using System.Web.Http;
 
 namespace Swashbuckle.Dummy.Controllers
 {
+    [RoutePrefix("FileDownload")]
     public class FileDownloadController : ApiController
     {
+        [HttpGet]
         public HttpResponseMessage GetFile()
         {
             var response = new HttpResponseMessage();
             var stream = new FileStream(@"c:\test.txt", FileMode.Open);
             response.Content = new StreamContent(stream);
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            return response;
+        }
+
+        [HttpGet]
+        [Route("filename")]
+        public HttpResponseMessage GetFileWithFilename()
+        {
+            var response = new HttpResponseMessage();
+            var stream = new FileStream(@"c:\test.txt", FileMode.Open);
+            response.Content = new StreamContent(stream);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "test.txt"
+            };
             return response;
         }
     }
